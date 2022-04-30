1c11182502bd0ef97a5489688083aed62f04d882
Jedidiah Oladele <jehddie@gmail.com>
2022-04-30 04:17:02 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,91 @@
+import streamlit as st
+from myfunctions import myvideo
+import datetime
+
+st.set_page_config(layout='wide')
+st.markdown("<h1 style='text-align:center'>YouTube Downloader</h1>", unsafe_allow_html=True)
+
+
+url = st.text_input(label='', placeholder='Paste video or playlist URL')
+#url = 'https://youtu.be/Kq5iPtAc_3I'
+url = 'https://youtube.com/playlist?list=PLbHrOSG7nVN1z4XoLX7_RC-WkgZHc3tnV'
+# Checks if the url posted is a playlist
+is_playlist = url[20:28] == 'playlist'
+
+
+
+
+def display_video(title, thumbnail, lenght, stream_prop):
+    """Displays detials about individual video"""
+
+    video_container = st.container()
+    # To keep things tidy
+    with video_container:
+        left_col, right_col = st.columns(2)
+
+        # Image, title and video lenght
+        left_col.image(thumbnail, width=500)
+        right_col.write(f"<h5>{title}</h5>", unsafe_allow_html=True)
+        right_col.write(f"<h5>{datetime.timedelta(seconds=lenght)}</h5>", unsafe_allow_html=True)
+        
+        # Create radio buttons with resolution and filesize
+        download_options = []
+        for p in stream_prop.values():
+            res = p[0].strip("\"")
+            size = round(p[1]/(1024*1024), 1)
+            download_options.append(f"{res} {size}mb")
+
+        video_res = '360p'
+        def set_vid_res(option):
+            video_res = option.split(' ')[0]
+
+        for download_option in download_options:
+            download_button = right_col.button(label=download_option, on_click=set_vid_res(download_option))
+            
+            
+        
+    
+    # If the download button was clicked or not
+    return download_button, video_res
+
+
+
+
+def make_video(url):
+    """Handles video creation, and download"""
+    myvideo_obj = myvideo(url)
+
+    # If an error occurs, display it and return
+    if myvideo_obj.error_message != None:
+        st.error(myvideo_obj.error_message)
+        return
+    else:
+        # Get video details and display them
+        video_details = myvideo_obj.get_video_details()
+        download_button, video_res = display_video(video_details['title'], video_details['thumbnail'],
+                                                    video_details['lenght'], video_details['stream_prop'])
+        
+        if download_button or download_all:
+            myvideo_obj.download_video(video_res)
+        
+
+
+download_all = False
+
+# Multiple videos
+if is_playlist and url:
+    st.progress(70)
+    video_urls = myvideo.get_playlist_videos(url)
+
+    if video_urls == None:
+        st.error('No playlist was found')
+    else:
+        download_all = st.button(label=f'Download all {len(video_urls)} videos')
+        # TODO Include total size and lenght
+        for url in video_urls:
+            make_video(url)
+
+# Single video
+elif url:
+    st.progress(70)
+    make_video(url)
---
+++ myfunctions.py
@@ -0,0 +1,69 @@
+from pytube import YouTube, Playlist, exceptions
+
+class myvideo:
+
+
+    def __init__(self, video_url):
+        """Create pytube YouTube object from url"""
+        self.video_url = video_url
+
+        # Error handling, with meaningful feedbacks
+        self.error_message = None
+        try:
+            self.video = YouTube(self.video_url)
+        except exceptions.RegexMatchError:
+            self.error_message = "No video was found"
+        except exceptions.AgeRestrictedError:
+            self.error_message = "Video is age restricted"
+        except exceptions.LiveStreamError:
+            self.error_message = "Video is a live stream"
+        except exceptions.VideoPrivate:
+            self.error_message = "Video is private"
+        except:
+            self.error_message = "An unexpected error occured \nPlease make a report"
+        
+
+
+    def get_video_details(self):
+        """Extracts and returns details about a video"""
+        
+        video_title = self.video.title
+        video_thumbnail = self.video.thumbnail_url
+        video_lenght = self.video.length
+        # TODO add mp3 format
+        streams = self.video.streams.filter(progressive=True)
+
+        # There's probabaly an easier way to parse this html-like stream object
+        # Dict key is the unique stream itag, and the value is a list containing stream resolution and filesize
+        stream_prop = {}
+        for stream in streams:
+            s = str(stream).lstrip('<Stream: ').rstrip('>').replace(' ', ', ').replace('=', ':').split(', ')
+            s = [var.split(':') for var in s]
+            s = dict(s)
+            stream_prop[s['itag']] = [s['res'], stream.filesize]
+
+
+        return {'title':video_title, 'thumbnail':video_thumbnail, 'lenght':video_lenght, 'stream_prop':stream_prop}
+
+
+
+    def download_video(self, resolution):
+            """Download and return a video file"""
+
+            # TODO choose resolution option
+            # TODO filename_prefix
+            file = self.video.streams.get_by_resolution(resolution).download()
+            return file
+
+
+
+    def get_playlist_videos(playlist_link):
+        """Return a list of all video urls from a playlist"""
+        
+        try:
+            p = Playlist(playlist_link)
+        except:
+            return None
+
+        video_urls = [url for url in p.video_urls]
+        return video_urls
