8a1a269596dee63e09392c10505fd973dd8ad184
Jedidiah Oladele <jehddie@gmail.com>
2022-05-03 19:40:34 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -8,7 +8,9 @@ st.markdown("<h1 style='text-align:center'>YouTube Downloader</h1>", unsafe_allo
 
 url = st.text_input(label='', placeholder='Paste video or playlist URL')
 #url = 'https://youtu.be/Kq5iPtAc_3I'
+#url = 'https://youtu.be/cwk417UfnTU'
 #url = 'https://youtube.com/playlist?list=PLbHrOSG7nVN1z4XoLX7_RC-WkgZHc3tnV'
+url = 'https://youtube.com/playlist?list=PLVD3APpfd1tuZG0pek_JaPYRhGwpJj3Jd'
 # Checks if the url posted is a playlist
 is_playlist = url[20:28] == 'playlist'
 
@@ -26,7 +28,15 @@ def display_video(title, thumbnail, lenght, stream_prop):
         # Image, title and video lenght
         left_col.image(thumbnail, width=500)
         right_col.write(f"<h5>{title}</h5>", unsafe_allow_html=True)
-        right_col.write(f"<h5>{datetime.timedelta(seconds=lenght)}</h5>", unsafe_allow_html=True)
+
+        # This complex logic is just to display the video lenght in a prettier format
+        timedelta_obj = datetime.timedelta(seconds=lenght)
+        time = str(timedelta_obj).split(sep=':')
+        if int(time[-3]) > 0:
+            right_col.write(f"<h5>{timedelta_obj}</h5>", unsafe_allow_html=True)
+        else:
+            right_col.write(f"<h5>{time[-2]}:{time[-1]}</h5>", unsafe_allow_html=True)
+
         
         # Create radio buttons with resolution and filesize
         download_options = []
@@ -35,9 +45,10 @@ def display_video(title, thumbnail, lenght, stream_prop):
             size = round(p[1]/(1024*1024), 1)
             download_options.append(f"{res} {size}mb")
 
-        video_res = '360p'
+        # TODO current playlist default
+        download_res = '360p'
         def set_vid_res(option):
-            video_res = option.split(' ')[0]
+            download_res = option.split(' ')[0]
 
         for download_option in download_options:
             download_button = right_col.button(label=download_option, on_click=set_vid_res(download_option))
@@ -46,14 +57,18 @@ def display_video(title, thumbnail, lenght, stream_prop):
         
     
     # If the download button was clicked or not
-    return download_button, video_res
+    return download_button, download_res
 
 
 
 
 def make_video(url):
     """Handles video creation, and download"""
-    myvideo_obj = myvideo(url)
+
+    #@st.cache(allow_output_mutation=True)
+    def get_video_obj(url):
+        return myvideo(url)
+    myvideo_obj = get_video_obj(url)
 
     # If an error occurs, display it and return
     if myvideo_obj.error_message != None:
@@ -62,30 +77,70 @@ def make_video(url):
     else:
         # Get video details and display them
         video_details = myvideo_obj.get_video_details()
-        download_button, video_res = display_video(video_details['title'], video_details['thumbnail'],
+        download_button, download_res = display_video(video_details['title'], video_details['thumbnail'],
                                                     video_details['lenght'], video_details['stream_prop'])
-        
-        if download_button or download_all:
-            myvideo_obj.download_video(video_res)
-        
 
 
-download_all = False
+        return {'myvideo_obj': myvideo_obj,
+                'download_button': download_button,
+                'download_res': download_res}
+
+
+
 
 # Multiple videos
 if is_playlist and url:
-    st.progress(70)
-    video_urls = myvideo.get_playlist_videos(url)
+    
+    # Get individual video URLs
+    with st.spinner('Extracting playlist'):
 
-    if video_urls == None:
+        @st.cache(allow_output_mutation=True, show_spinner=False)
+        def get_playlist(url):
+             return myvideo.get_playlist_videos(url)
+        playlist = get_playlist(url)
+        
+
+    if playlist == None or playlist == []:
         st.error('No playlist was found')
+
     else:
-        download_all = st.button(label=f'Download all {len(video_urls)} videos')
-        # TODO Include total size and lenght
-        for url in video_urls:
-            make_video(url)
+        video_urls = playlist['urls']
+        total_lenght = playlist.get('lenght', 0)
+
+        # Display playlist information
+        # TODO Include total size
+        col1, col2 = st.columns(2)
+        download_all = col2.button(label=f'Download all')
+        with col1:
+            # This complex logic is just to display the video lenght in a prettier format
+            timedelta_obj = datetime.timedelta(seconds=total_lenght)
+            time = str(timedelta_obj).split(sep=':')
+            if int(time[-3]) > 0:
+                st.write(f'{len(video_urls)} videos found {timedelta_obj} long')
+            else:
+                st.write(f'{len(video_urls)} videos found {time[-2]}:{time[-1]} long')
+
+
+        
+        with st.spinner('Fetching videos'):
+            for url in video_urls:
+                vids = make_video(url)
+        # Download clicked videos
+        for vid in vids:
+            if vid['download_button']:
+                vid['myvideo_obj'].download_video(vid['video_res'])
+        # Download all videos
+        if download_all:
+            for vid in vids:
+                vid['myvideo_obj'].download_video(vid['video_res'])
+
 
 # Single video
 elif url:
-    st.progress(70)
-    make_video(url)
+    with st.spinner('Fetching video'):
+        vid = make_video(url)
+    
+    if vid['download_button']:
+        vid['myvideo_obj'].download_video(vid['video_res'])
+        
+
